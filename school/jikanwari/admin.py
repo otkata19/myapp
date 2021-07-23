305736b6edd82285c20e11ba59e440d568550a83
otkata19 <1968kocm1akouk@gmail.com>
2021-07-23 11:27:51 +0900

    DB作成、検索して画面に表示する

--- school/jikanwari/admin.py
+++ school/jikanwari/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SampleDB # models.pyで指定したクラス名
 
 # Register your models here.
+admin.site.register(SampleDB) # models.pyで指定したクラス名
--- school/jikanwari/views.py
+++ school/jikanwari/views.py
@@ -1,8 +1,18 @@
 # Create your views here.
 from django.shortcuts import render  
-from django.views import View  
+from django.views import View
+from .models import SampleDB
 
 class SampleView(View):  
     def get(self, request, *args, **kwargs):  
-        return render(request, 'jikanwari/top_page.html')
+        return render(request, 'jikanwari/page01.html')
+
+    def post(self, request, *args, **kwargs):  
+        input_data = request.POST['input_data']
+        result = SampleDB.objects.filter(sample2=input_data)
+        result_sample1 = result[0].sample1
+        result_sample2 = result[0].sample2
+        context={'result_sample1':result_sample1, 'result_sample2':result_sample2}
+        return render(request, 'jikanwari/page02.html', context=context,)
+
 top_page = SampleView.as_view()
